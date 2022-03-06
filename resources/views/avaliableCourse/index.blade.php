71092be327a442ab918356e9ae2a2cf10f083ef3
Junn Eric Timoteo <juntyms@yahoo.com>
2022-03-06 14:29:42 +0400

    updated some spelling

--- resources/views/avaliableCourse/index.blade.php
+++ resources/views/avaliableCourse/index.blade.php
@@ -3,68 +3,66 @@
 Avaliable courses
 @endsection
 @section('PageTitle')
-    <h3>{{Auth::user()->department->name}}: avaliable courses</h3>
-      <nav>
-        <ol class="breadcrumb">
-          <li class="breadcrumb-item"><a href="{{route('home')}}">Dashboard</a></li>
-          <li class="breadcrumb-item"><a href="{{route('Acourse.index')}}">Avaliable course</a></li>
-        </ol>
-      </nav>
-  
+<h3>{{Auth::user()->department->name}}: Avaliable Courses</h3>
+<nav>
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item"><a href="{{route('home')}}">Dashboard</a></li>
+    <li class="breadcrumb-item"><a href="{{route('Acourse.index')}}">Avaliable course</a></li>
+  </ol>
+</nav>
+
 @endsection
 @section('content')
 <div class="row">
 
   <div class="col-lg-12">
 
-      <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">
-              <a href="{{route('Addcourse.index')}}" class="btn btn-success btn-sm" role="button" aria-pressed="true">
-                  Add Avaliable course
-                </a>
-              </h5>
-              <!-- Table with stripped rows -->
-              <div class="table-responsive">
-                <table class="table datatable table-hover">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">
+          <a href="{{route('Addcourse.index')}}" class="btn btn-success btn-sm" role="button" aria-pressed="true">
+            Add Avaliable course
+          </a>
+        </h5>
+        <!-- Table with stripped rows -->
+        <div class="table-responsive">
+          <table class="table datatable table-hover">
 
-                    <thead>
-                    <tr>
-                        <th>Course</th>
-                        <th>Tutor </th>
-                        <th>Department</th>
-                        <th>location</th>
-                        <th>Date</th>
-                        <th>Time</th>
-                        <th>Action</th>
-                        
-                    </tr>
-                    </thead>
-                    <tbody>
-                            @foreach($Dep_Courses as $Dep_Course)
-                                <tr>
-                                    <td>{{$Dep_Course->course->name }}</td>
-                                    <td>{{$Dep_Course->tutor->gettutorname->fullname }}</td>
-                                    <td>{{$Dep_Course->course->departmentname->name }} </td>
-                                    <td>{{$Dep_Course->location}}</td>         
-                                    <td>{{$Dep_Course->day}}</td>
-                                    <td>{{$Dep_Course->time}}:00</td>
-                                    <td>
-                                      <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#course_delete{{$Dep_Course->id}}" title="Delete course"><i class="bi bi-exclamation-triangle"></i></button>
-                                    </td>
-                              
-                                    <div class="modal fade" id="course_delete{{$Dep_Course->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                        @include('avaliableCourse.DeleteAVCourse')
-                                    </div>
-                                </tr>
-                            @endforeach
-                    </tbody>
-                </table> 
-              </div>               
-            </div>
+            <thead>
+              <tr>
+                <th>Course</th>
+                <th>Tutor </th>
+                <th>Department</th>
+                <th>location</th>
+                <th>Date</th>
+                <th>Time</th>
+                <th>Action</th>
+
+              </tr>
+            </thead>
+            <tbody>
+              @foreach($Dep_Courses as $Dep_Course)
+              <tr>
+                <td>{{$Dep_Course->course->name }}</td>
+                <td>{{$Dep_Course->tutor->gettutorname->fullname }}</td>
+                <td>{{$Dep_Course->course->departmentname->name }} </td>
+                <td>{{$Dep_Course->location}}</td>
+                <td>{{$Dep_Course->day}}</td>
+                <td>{{$Dep_Course->time}}:00</td>
+                <td>
+                  <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#course_delete{{$Dep_Course->id}}" title="Delete course"><i class="bi bi-exclamation-triangle"></i></button>
+                </td>
+
+                <div class="modal fade" id="course_delete{{$Dep_Course->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                  @include('avaliableCourse.DeleteAVCourse')
+                </div>
+              </tr>
+              @endforeach
+            </tbody>
+          </table>
         </div>
+      </div>
+    </div>
   </div>
 </div>
 @endsection
-
-                                        
--- resources/views/student/booking/index.blade.php
+++ resources/views/student/booking/index.blade.php
@@ -3,35 +3,33 @@
 Booking Select Department
 @endsection
 @section('PageTitle')
-    <h3>booking Tutorial: Select Department </h3>
-      <nav>
-        <ol class="breadcrumb">
-          <li class="breadcrumb-item"><a href="{{route('home')}}">Dashboard</a></li>
-          <li class="breadcrumb-item"><a href="{{route('student.booking.Department')}}">Booking</a></li>
-        </ol>
-      </nav>
-    
+<h3>booking Tutorial: Select Department </h3>
+<nav>
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item"><a href="{{route('home')}}">Dashboard</a></li>
+    <li class="breadcrumb-item"><a href="{{route('student.booking.Department')}}">Booking</a></li>
+  </ol>
+</nav>
+
 @endsection
 @section('content')
 <div class="row">
-    @foreach($deps as $dep)
+  @foreach($deps as $dep)
 
-      @if($dep->Available()->where('available_courses.active',1)->count()>0)
+  @if($dep->Available()->where('available_courses.active',1)->count()>0)
 
-        <a class="col-md-6 col-xl-3"  href="{{route('deplistAv',$dep)}}">
-            <div class="card bg-c-blue order-card">
-                <div class="card-block">
-                    <h6 class="m-b-20">{{$dep->name}} </h6>
-                    <h4 class="text-right"><i class="bi bi-journal-text f-left"></i><span>Turtorials</span></h4>
-                    <p class="m-b-0"><span class="f-right">{{$dep->Available()->where('available_courses.active',1)->count()}}</span></p>
-                </div>
-            </div>
-        </a>
-      @endif
-    @endforeach
+  <a class="col-md-6 col-xl-3" href="{{route('deplistAv',$dep)}}">
+    <div class="card bg-c-blue order-card">
+      <div class="card-block">
+        <h6 class="m-b-20">{{$dep->name}} </h6>
+        <h4 class="text-right"><i class="bi bi-journal-text f-left"></i><span>Tutorials</span></h4>
+        <p class="m-b-0"><span class="f-right">{{$dep->Available()->where('available_courses.active',1)->count()}}</span></p>
+      </div>
+    </div>
+  </a>
+  @endif
+  @endforeach
 </div>
 
-                   
-@endsection
 
-                                        
+@endsection
