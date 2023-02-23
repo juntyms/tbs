f1760a83d1154927b30f3b426eb2952858a39e8b
Musallam Al Mashani <musallam.almshani@gmail.com>
2023-02-23 09:41:29 +0400

    add utas logo

--- resources/views/layouts/main-header.blade.php
+++ resources/views/layouts/main-header.blade.php
@@ -1,59 +1,58 @@
 <header id="header" class="header fixed-top d-flex align-items-center">
 
     <div class="d-flex align-items-center justify-content-between">
-      <a href="{{route('home')}}" class="logo d-flex align-items-center">
-        <img src="{{ URL::asset('assets/img/logo-dark.png') }}" alt="">
-        <span class="d-none d-lg-block">UTAS-TBS</span>
-      </a>
-      <i class="bi bi-list toggle-sidebar-btn"></i>
+        <a href="{{route('home')}}" class="logo d-flex align-items-center">
+            <img src="{{ URL::asset('assets/img/utasll.png') }}" alt="">
+            <span class="d-none d-lg-block">UTAS-TBS</span>
+        </a>
+        <i class="bi bi-list toggle-sidebar-btn"></i>
     </div><!-- End Logo -->
 
 
     <nav class="header-nav ms-auto">
-      <ul class="d-flex align-items-center">
+        <ul class="d-flex align-items-center">
 
-        <li class="nav-item dropdown pe-3">
+            <li class="nav-item dropdown pe-3">
 
-          <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-            <img src="{{asset('/storage/images/'.Auth::user()->photo)}}" alt="Profile" class="rounded-circle">
-            <span class="d-none d-md-block dropdown-toggle ps-2">{{ Auth::user()->name }}</span>
-          </a><!-- End Profile Iamge Icon -->
+                <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
+                    <img src="{{asset('/storage/images/'.Auth::user()->photo)}}" alt="Profile" class="rounded-circle">
+                    <span class="d-none d-md-block dropdown-toggle ps-2">{{ Auth::user()->name }}</span>
+                </a><!-- End Profile Iamge Icon -->
 
-          <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
-            <li class="dropdown-header">
-              <h6>{{ Auth::user()->email }}</h6>
-             
-            </li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
+                <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
+                    <li class="dropdown-header">
+                        <h6>{{ Auth::user()->email }}</h6>
 
-            <li>
-              <a class="dropdown-item d-flex align-items-center" href="{{route('user.profile')}}">
-                <i class="bi bi-person"></i>
-                <span>My Profile</span>
-              </a>
-            </li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
+                    </li>
+                    <li>
+                        <hr class="dropdown-divider">
+                    </li>
 
-            <li>
-              <a class="dropdown-item d-flex align-items-center" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
+                    <li>
+                        <a class="dropdown-item d-flex align-items-center" href="{{route('user.profile')}}">
+                            <i class="bi bi-person"></i>
+                            <span>My Profile</span>
+                        </a>
+                    </li>
+                    <li>
+                        <hr class="dropdown-divider">
+                    </li>
+
+                    <li>
+                        <a class="dropdown-item d-flex align-items-center" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                <i class="bi bi-box-arrow-right"></i>
-                <span>Sign Out</span>
-              </a>
-              <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                @csrf
-              </form>
-            </li>
+                            <i class="bi bi-box-arrow-right"></i>
+                            <span>Sign Out</span>
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </li>
 
-          </ul><!-- End Profile Dropdown Items -->
-        </li><!-- End Profile Nav -->
+                </ul><!-- End Profile Dropdown Items -->
+            </li><!-- End Profile Nav -->
 
-      </ul>
+        </ul>
     </nav><!-- End Icons Navigation -->
 
-  </header><!-- End Header -->
+</header><!-- End Header -->
